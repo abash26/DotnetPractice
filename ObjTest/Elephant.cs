46705b7055f0e28e06d11defc01cb20e3d6e9811
banastas <hu.bash26@gmail.com>
2025-02-27 16:26:35 +0100

    Add project files.

---
+++ ObjTest/Elephant.cs
@@ -0,0 +1,13 @@
+﻿namespace ObjTest;
+
+internal class Elephant
+{
+    public string Name = "";
+    public int EarSize;
+
+    public void WhoAmI()
+    {
+        Console.WriteLine($"My name is {Name}");
+        Console.WriteLine($"My ears are {EarSize} inches tall");
+    }
+}
---
+++ ObjTest/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ObjTest;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Guy joe = new Guy() { Cash = 50, Name = "Joe" };
+        Guy bob = new Guy() { Cash = 100, Name = "Bob" };
+
+        if (0.1F + 0.2F == 0.3F) Console.WriteLine("They're equal");
+        else Console.WriteLine("They aren't equal");
+
+        Elephant lucinda = new Elephant() { Name = "Lucinda", EarSize = 33 };
+        Elephant lloyd = new Elephant() { Name = "Lloyd", EarSize = 40 };
+
+        while (true)
+        {
+            Console.WriteLine("Press 1 for Lloyd, 2 for Lucinda, 3 to swap");
+            var input = Console.ReadLine();
+            if (input == "1")
+            {
+                Console.WriteLine("You pressed 1");
+                Console.WriteLine("Calling lloyd.WhoAmI()");
+                lloyd.WhoAmI();
+            }
+            else if (input == "2")
+            {
+                Console.WriteLine("You pressed 2");
+                Console.WriteLine("Calling lucinda.WhoAmI()");
+                lucinda.WhoAmI();
+            }
+            else if (input == "3")
+            {
+                (lucinda, lloyd) = (lloyd, lucinda);
+                Console.WriteLine("References have been swapped");
+            } else return;
+        }
+    }
+}
